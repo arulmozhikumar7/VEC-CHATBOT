f67e4717da02bda92d6804f3885f334fe5c2c667
Arulmozhikumar <arulmozhikumar7@gmail.com>
2024-04-19 00:50:19 +0530

    Changed File Name

--- vec-chatbot-server/index.js
+++ vec-chatbot-server/index.js
@@ -7,11 +7,7 @@ const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+app.use(cors());
 app.use(bodyParser.json());
 
 const responses = JSON.parse(fs.readFileSync("responses.json", "utf-8"));
